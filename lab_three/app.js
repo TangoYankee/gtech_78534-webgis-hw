37d060d74c747771b850aac85592d62a815f4e79
tangoyankee <miller.tim108@gmail.com>
2023-03-20 18:49:20 -0400

    dynamically color based on population

--- lab_three/app.js
+++ lab_three/app.js
@@ -1,3 +1,26 @@
+const styleTractDemograpics = (feature)=> {
+    const totalPopulation = feature.properties.total;
+    let fillColor = null;
+
+    if (totalPopulation < 100) {
+        fillColor = "#f1eef6"
+    } else if (totalPopulation < 1000) {
+        fillColor =  '#bdc9e1' 
+    } else if (totalPopulation < 5000) {
+        fillColor = '#74a9cf'
+    } else if (totalPopulation < 7500) {
+        fillColor = '#2b8cbe'
+    } else {
+        fillColor = "#045a8d"
+    }
+    return { 
+        fillColor,
+        color: "#222347",
+        fillOpacity: .75,
+        weight: 0.75,
+        opacity: .75,
+    }
+}
 addEventListener('DOMContentLoaded', async () => {
     const map = L.map('map').setView([40.74, -74.0], 13);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -9,7 +32,6 @@ addEventListener('DOMContentLoaded', async () => {
     const tractDemographics = await response.json();
 
     L.geoJSON(tractDemographics, {
-        fillColor: "#ff0000",
-        fillOpacity: 0.5 
+        style: styleTractDemograpics
     }).addTo(map);
 });
