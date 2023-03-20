8575775469bee41e246ffe04d37305e00e6a833c
tangoyankee <miller.tim108@gmail.com>
2023-03-20 17:49:14 -0400

    listen for content loaded

--- lab_three/app.js
+++ lab_three/app.js
@@ -1,5 +1,7 @@
-const map = L.map('map').setView([40.74, -74.0], 13);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+addEventListener('DOMContentLoaded', () => {
+    const map = L.map('map').setView([40.74, -74.0], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+})
